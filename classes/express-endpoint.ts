f7542a8e26860a915fa11d2f8d07a98567b0fc52
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-07-27 22:03:12 -0700

    Update express-endpoint.ts

--- classes/express-endpoint.ts
+++ classes/express-endpoint.ts
@@ -1,39 +1,47 @@
-export namespace ExpressEndpoint {
-    enum Method {
-        GET = "GET",
-        POST = "POST",
-        HEAD = "HEAD",
-        PUT = "PUT",
-        PATCH = "PATCH",
-        DELETE = "DELETE",
-        CONNECT = "CONNECT",
-        OPTIONS = "OPTIONS",
-        TRACE = "TRACE",
+import {Request, Response} from "express";
+
+export enum Method {
+    GET = "GET",
+    POST = "POST",
+    HEAD = "HEAD",
+    PUT = "PUT",
+    PATCH = "PATCH",
+    DELETE = "DELETE",
+    CONNECT = "CONNECT",
+    OPTIONS = "OPTIONS",
+    TRACE = "TRACE",
+}
+
+export class ExpressEndpoint {
+    // Method for which the endpoint will listen
+    method: Method;
+
+    // URL path at which the endpoint will listen
+    path: string;
+
+    // Whether the endpoint should actually respond to requests
+    active: boolean;
+
+    // Function that will handle endpoint requests
+    handler: (req: Request, res: Response, next: Function) => void;
+
+    // Require authorized JSON Web Token
+    authenticated: boolean;
+
+    // Allowed requests per minute (integer)
+    rateLimit: number;
+
+    constructor(method: Method, path: string = null, active: boolean, authenticated: boolean = false, rateLimit: number = 0) {
+        this.method = method;
+        this.path = path;
+        this.active = active;
+        this.authenticated = authenticated;
+        this.rateLimit = rateLimit;
     }
 
-    class Endpoint {
-        // Method for which the endpoint will listen
-        method: Method;
-
-        // URL path at which the endpoint will listen
-        path: string;
-
-        // Function that will handle endpoint requests
-        handler: Function;
-
-        // Require authorized JSON Web Token
-        authenticated: boolean;
-
-        // Allowed requests per minute (integer)
-        rateLimit: number;
-
-        constructor(method: Method, path: string, handler: Function, authenticated: boolean = false, rateLimit: number = 0) {
-            this.method = method;
-            this.path = path;
-            this.handler = handler;
-            this.authenticated = authenticated;
-            this.rateLimit = rateLimit;
-        }
-
+    setHandler(handler: (req: Request, res: Response, next: Function) => void) : ExpressEndpoint {
+        this.handler = handler;
+        return this;
     }
+
 }
