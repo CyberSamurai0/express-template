332f3c6ac540494d247d7279426b24fa8ddfa14f
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-02 23:34:51 -0700

    Import logs library

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as Express from "express";
 import EndpointLoader from "./endpoint-loader";
+import {warn, info, ok, err, power} from "./log";
 
 let app : Express.Express = Express();
 EndpointLoader(app);
