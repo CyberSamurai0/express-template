b66cbe6d98871502370ef0c71f59f14d5c8747e7
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-02 23:36:19 -0700

    Allow server to bind to port

--- index.ts
+++ index.ts
@@ -3,4 +3,15 @@ import EndpointLoader from "./endpoint-loader";
 import {warn, info, ok, err, power} from "./log";
 
 let app : Express.Express = Express();
+
+const port : string = process.env.SERVER_PORT || "3000";
+
+
+// Invoke the endpoint loader (./endpoint-loader.ts)
 EndpointLoader(app);
+
+
+// Bind server to listening port
+app.listen(port, () => {
+    power(`App listening at http://${process.env.SERVER_IP}:${port}`);
+});
