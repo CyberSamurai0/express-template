89f1e485d04edd83c272520d1a66c2527ce4763a
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-03 15:24:20 -0700

    Add sanitization to logging middleware
    Resolves #1

--- index.ts
+++ index.ts
@@ -10,11 +10,32 @@ const port : string = process.env.SERVER_PORT || "3000";
 // TODO add other middleware for handling tokens and etc.
 
 
-// Log all requests
+// Validate req.method as a standard HTTP method
+function validateMethod(req: Express.Request, res: Express.Response, next: Express.NextFunction) {
+    // Whitelist standard methods for input validation
+    const methods : string[] = ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE", "CONNECT", "OPTIONS", "TRACE"];
+
+    // Overwrite field with reliably sanitized value
+    req.method = methods.includes(req.method) ? req.method : "UNKNOWN";
+
+    // Call next middleware
+    return next();
+}
+app.use(validateMethod);
+
+
+// Log all incoming requests to the console.
 function logAccess(req : Express.Request, res : Express.Response, next: Express.NextFunction) {
-    console.log();
-    // TODO this is unsanitized input
-    info(`"${req.method} ${req.path} ${req.protocol.toUpperCase()}/${req.httpVersion}" "${req.get("User-Agent")}"`)
+    // URL-encode other user generated fields
+    let _path : string = encodeURI(req.path);
+    let _protocol : string = encodeURI(req.protocol).toUpperCase();
+    let _httpVersion : string = encodeURI(req.httpVersion);
+    let _userAgent : string = encodeURI(req.get("User-Agent"));
+
+    // console.log(); // Newline - not really needed?
+    info(`"${req.method} ${_path} ${_protocol}/${_httpVersion}" "${_userAgent}"`);
+
+    // Call next middleware
     return next();
 }
 app.use(logAccess);
@@ -27,8 +48,8 @@ EndpointLoader(app);
 // Fallback to 404 if no other handlers match.
 // THIS MUST BE THE FINAL MIDDLEWARE
 function fallback404(req : Express.Request, res : Express.Response, next: Express.NextFunction) {
-    // TODO this is unsanitized input
-    info(`${req.method} ${req.path} not found, returning 404`);
+    let _path : string = encodeURI(req.path);
+    info(`${req.method} ${_path} not found, returning 404`);
     res.sendStatus(404);
 }
 app.use(fallback404);
