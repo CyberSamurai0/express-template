d5021775beb80ace1136a494d00408507babdffa
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-02 23:36:51 -0700

    Log all requests

--- index.ts
+++ index.ts
@@ -7,6 +7,19 @@ let app : Express.Express = Express();
 const port : string = process.env.SERVER_PORT || "3000";
 
 
+// TODO add other middleware for handling tokens and etc.
+
+
+// Log all requests
+function logAccess(req : Express.Request, res : Express.Response, next: Express.NextFunction) {
+    console.log();
+    // TODO this is unsanitized input
+    info(`"${req.method} ${req.path} ${req.protocol.toUpperCase()}/${req.httpVersion}" "${req.get("User-Agent")}"`)
+    return next();
+}
+app.use(logAccess);
+
+
 // Invoke the endpoint loader (./endpoint-loader.ts)
 EndpointLoader(app);
 
