3b9189bc646401133ba14a4f3ad8d84ca44afd84
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-03 15:37:49 -0700

    Add more detailed usage explanation

--- endpoints/get/users/user-id.js
+++ endpoints/get/users/user-id.js
@@ -1,8 +1,12 @@
 const {ExpressEndpoint} = require("../../../classes/express-endpoint");
 
 
-
-// Add function arguments as needed
+/**
+ * Export a wrapper function that returns a compiled endpoint handler.
+ *
+ * This allows us to add objects to the handler's scope at runtime
+ * without inserting them as arguments within the handler itself.
+ */
 module.exports = (args) => {
     let database = args[0]; // Example - pass in a reference to the database object for querying
 
