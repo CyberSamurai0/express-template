ea0e1631010d68e09318e0d6fe20d168d84241fd
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-03 16:02:27 -0700

    Update user-id.ts

--- endpoints/get/users/user-id.ts
+++ endpoints/get/users/user-id.ts
@@ -6,6 +6,10 @@ import {NextFunction, Request, Response} from "express";
  *
  * This allows us to add objects to the handler's scope at runtime
  * without inserting them as arguments within the handler itself.
+ *
+ * Note that we're using `export =` rather than `export default`.
+ * This is to provide compatibility with CommonJS require() for
+ * inline ExpressEndpoint definitions.
  */
 export = (args: Object[]) => {
     let database : Object = args[0]; // Example - pass in a reference to the database object for querying
