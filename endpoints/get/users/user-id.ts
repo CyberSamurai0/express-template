775fd4146ff1703b80e87320f441874b9eb91948
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-08-03 15:59:42 -0700

    Convert user-id to TypeScript

---
+++ endpoints/get/users/user-id.ts
@@ -0,0 +1,18 @@
+import {ExpressEndpoint} from "../../../classes/express-endpoint";
+import {NextFunction, Request, Response} from "express";
+
+/**
+ * Export a wrapper function that returns a compiled endpoint handler.
+ *
+ * This allows us to add objects to the handler's scope at runtime
+ * without inserting them as arguments within the handler itself.
+ */
+export = (args: Object[]) => {
+    let database : Object = args[0]; // Example - pass in a reference to the database object for querying
+
+    return (req: Request, res: Response, next: NextFunction) => {
+        const _userID: string = encodeURI(req.params["userID"]);
+
+        if (_userID === "1") res.sendStatus(200); // If GET /users/1 then respond with 200 OK
+    }
+}
